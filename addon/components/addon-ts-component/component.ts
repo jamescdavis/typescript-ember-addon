36dbce5f7563373a1be74c6a1d43399d1669eecd
James C. Davis <james@cos.io>
2018-02-20 13:40:05 -0500

    Ignore import of compiled template

--- addon/components/addon-ts-component/component.ts
+++ addon/components/addon-ts-component/component.ts
@@ -1,4 +1,5 @@
 import Component from '@ember/component';
+// @ts-ignore: Ignore import of compiled template
 import layout from './template';
 
 export default class AddonTsComponent extends Component.extend({
